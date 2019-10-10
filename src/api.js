3fe9d95d5c4c7f537a28c50fd998bca606441c42
Yuranuch <yura477573@mail.ru>
2019-10-10 14:59:17 +0300

    create instance

--- src/api.js
+++ src/api.js
@@ -1,9 +1,14 @@
 import * as axios from "axios";
 
+const instance = axios.create({
+    baseURL: "https://social-network.samuraijs.com/api/1.0/todo-lists",
+    withCredentials: true,
+    headers: {"API-KEY": "2712bbc4-99c4-4494-954c-6bd0564807d4"}
+})
+
 export const api = {
     createTask(todolistId, newText) {
-        return axios.post(`https://social-network.samuraijs.com/api/1.0/todo-lists/${todolistId}/tasks`, {title: newText},
-            {withCredentials: true, headers: {"API-KEY": "2712bbc4-99c4-4494-954c-6bd0564807d4"}})
+        return instance.post(`/${todolistId}/tasks`, {title: newText})
     },
     getTasks(todolistId) {
         return axios.get(`https://social-network.samuraijs.com/api/1.0/todo-lists/${todolistId}/tasks`,
